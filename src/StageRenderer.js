9b7d7c4b4a6707923cbb45105decba4739e5619f
Daniel Lewis <actiondan@gmail.com>
2017-03-09 16:11:39 +0000

    buffer optimization

--- src/StageRenderer.js
+++ src/StageRenderer.js
@@ -4,13 +4,21 @@ import {new_vertex_buffer, bind_vertex_buffer, get_program} from './webgl';
 var trn = vec3.create();
 var mat = mat4.create();
 
-const VERTEX_BUFFER_SIZE = 1 << 16;
+const VERTEX_BUFFER_SIZE = 1 << 20; // 1MB
 
 function alloc_buffer(size) {
     var buf = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, buf);
     gl.bufferData(gl.ARRAY_BUFFER, size, gl.STATIC_DRAW);
     return buf;
+}
+
+var buffer_switch_count = 0;
+var bound_buffer = null;
+
+function reset_buffer_switch() {
+    bound_buffer = null;
+    buffer_switch_count = 0;
 }
 
 class VertexBuffer {
@@ -31,6 +39,16 @@ class VertexBuffer {
         this.dp += src.length;
         return start;
     }
+
+    bind() {
+        if (bound_buffer === this)
+            return false;
+
+        bind_vertex_buffer(this.buffer);
+        bound_buffer = this;
+        ++buffer_switch_count;
+        return true;
+    }
 }
 
 export class StageRenderer {
@@ -38,7 +56,6 @@ export class StageRenderer {
         this.env = null;
         this.stage = null;
         this.buffers = [];
-        this.bound_buffer = null;
         this.pgm = null;
     }
 
@@ -54,8 +71,10 @@ export class StageRenderer {
         const VERTEX_SIZE = 24;
 
         for (;;) {
-            if (!this.buffers.length)
+            if (!this.buffers.length) {
                 this.buffers.push(new VertexBuffer);
+                console.log('vb:', this.buffers.length);
+            }
 
             var vb = this.buffers[this.buffers.length - 1];
             console.assert(obj.vertex_array.length <= vb.size);
@@ -67,6 +86,7 @@ export class StageRenderer {
                 break;
             } else {
                 this.buffers.push(new VertexBuffer);
+                console.log('vb:', this.buffers.length);
             }
         }
 
@@ -75,23 +95,6 @@ export class StageRenderer {
     }
 
     draw_tmd_object(obj, mat) {
-        /*
-        var vb = this.get_vertex_buffer(obj);
-        var pgm = this.pgm;
-
-        if (vb !== this.bound_buffer) {
-            bind_vertex_buffer(vb.buffer);
-            pgm.vertexAttribPointer('position', 3, gl.SHORT, false, 24, 0);
-            pgm.vertexAttribPointer('normal', 3, gl.SHORT, true, 24, 8);
-            pgm.vertexAttribPointer('color', 3, gl.UNSIGNED_BYTE, true, 24, 16);
-            pgm.vertexAttribPointer('texcoord', 2, gl.UNSIGNED_SHORT, false, 24, 20);
-            this.bound_buffer = vb;
-        }
-
-        pgm.uniformMatrix4fv('m_obj', mat);
-        gl.drawArrays(gl.TRIANGLES, obj.vertex_start, obj.vertex_count);
-        */
-
         if (!obj.vertex_buffer) {
             obj.vertex_buffer = new_vertex_buffer(obj.vertex_array);
         }
@@ -102,6 +105,19 @@ export class StageRenderer {
         pgm.vertexAttribPointer('normal', 3, gl.SHORT, true, 24, 8);
         pgm.vertexAttribPointer('color', 3, gl.UNSIGNED_BYTE, true, 24, 16);
         pgm.vertexAttribPointer('texcoord', 2, gl.UNSIGNED_SHORT, false, 24, 20);
+
+        pgm.uniformMatrix4fv('m_obj', mat);
+        gl.drawArrays(gl.TRIANGLES, obj.vertex_start, obj.vertex_count);
+    }
+
+    draw_tmd_object2(obj, mat) {
+        var pgm = this.pgm;
+        if (this.get_vertex_buffer(obj).bind()) {
+            pgm.vertexAttribPointer('position', 3, gl.SHORT, false, 24, 0);
+            pgm.vertexAttribPointer('normal', 3, gl.SHORT, true, 24, 8);
+            pgm.vertexAttribPointer('color', 3, gl.UNSIGNED_BYTE, true, 24, 16);
+            pgm.vertexAttribPointer('texcoord', 2, gl.UNSIGNED_SHORT, false, 24, 20);
+        }
 
         pgm.uniformMatrix4fv('m_obj', mat);
         gl.drawArrays(gl.TRIANGLES, obj.vertex_start, obj.vertex_count);
@@ -127,7 +143,7 @@ export class StageRenderer {
         var tmd_object = tmd.objects[tmd_index];
 
         //pgm.uniform3f('debug_color', (tile.collision & 0x80) ? 1 : 0, 0, 0);
-        this.draw_tmd_object(tmd_object, mat);
+        this.draw_tmd_object2(tmd_object, mat);
 
         if (tile.extra) {
             this.draw_tile(tmd, tile.extra, trn, tx, ty);
@@ -152,7 +168,7 @@ export class StageRenderer {
             return;
 
         var tix = stage.tix;
-        if (!tix.texture)
+        if (!(tix && tix.texture))
             return;
 
         var pgm = this.pgm = get_program('tmd').use();
@@ -165,7 +181,7 @@ export class StageRenderer {
         gl.enable(gl.DEPTH_TEST);
         gl.disable(gl.CULL_FACE);
 
-        this.bound_buffer = null;
+        reset_buffer_switch();
 
         for (var lbd_index = 0; lbd_index < stage.lbd_count; ++lbd_index) {
             var lbd = stage.lbds[lbd_index];
@@ -175,5 +191,7 @@ export class StageRenderer {
             stage.get_lbd_translation(trn, lbd_index);
             this.draw_lbd(lbd, trn);
         }
+
+        //console.log('buffer_switch_count:', buffer_switch_count);
     }
 }
