2f9445189b54372d3ed6c9d76ecad61dce27225a
Daniel Lewis <actiondan@gmail.com>
2017-04-11 15:47:49 +0100

    gulp fixes

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,40 @@
 var gulp = require('gulp');
 var usemin = require('gulp-usemin');
 var uglify = require('gulp-uglify');
+var cleanCSS = require('gulp-clean-css');
+var del = require('del');
+
+gulp.task('clean', function() {
+    return del([
+        'dist/**'
+    ]);
+});
+
+gulp.task('assets', function() {
+    var assets = [
+        'data/**',
+        'images/**',
+        'photos/**',
+        'sounds/**',
+        'gifs/**',
+        'fonts/**',
+    ];
+
+    gulp.src(assets, { cwd: 'static', base: 'static' })
+        .pipe(gulp.dest('dist'));
+});
+
  
 gulp.task('usemin', function() {
     return gulp.src('static/index.html')
         .pipe(usemin({
+            css: [ cleanCSS() ],
             js: [ uglify() ],
         }))
-        .pipe(gulp.dest('dist/'));
+        .pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', ['usemin']);
+gulp.task('default', [
+    'usemin',
+    'assets'
+]);
